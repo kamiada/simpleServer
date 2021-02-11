7c4e1e3cee2c7bced6a0662b902881cb55df4048
kamiada <kamiada528@gmail.com>
2021-02-11 17:08:59 +0000

    getting postman

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+Task = require('./api/models/todoListModel');
+bodyParser = require('body-parser');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencode({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
